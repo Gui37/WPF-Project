e95b8ce84d8a637a4849d3dc94ab168c91a8f6bf
Gui37 <guifty32@gmail.com>
2023-03-21 23:52:05 +0200

    Adicionar arquivos de projeto.

---
+++ DesignProject/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using DesignProject.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignProject.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand AppsViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+        public RelayCommand FeaturedViewCommand { get; set; }
+       
+        public HomeViewModel HomeVm { get; set; }
+        public AppsViewModel AppsVm { get; set; }
+        public DiscoveryViewModel DiscoveryVm { get; set; }
+        public FeaturedViewModel FeaturedVm { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+        HomeVm = new HomeViewModel();
+        AppsVm = new AppsViewModel();
+        DiscoveryVm = new DiscoveryViewModel();
+        FeaturedVm = new FeaturedViewModel();
+        CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o => { 
+                CurrentView = HomeVm;
+            });
+            AppsViewCommand = new RelayCommand(o => { 
+                CurrentView = AppsVm;
+            });
+            DiscoveryViewCommand = new RelayCommand(o => { 
+                CurrentView = DiscoveryVm;
+            });
+            FeaturedViewCommand = new RelayCommand(o => { 
+                CurrentView = FeaturedVm;
+            });
+
+        }
+    }
+}
